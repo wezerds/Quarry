9b94a16e9d0d207091f1c51feea2162a87e1b2da
BuildTools <electrohedric@null.spigotmc.org>
2015-09-09 15:00:39 -0400

    Revised the crafting recipes
    AddRecipes() Method now takes care of crafting recipes instead of
    everything inside the onEnable method()

--- src/me/cybercontroll/Quarry/Quarrymain.java
+++ src/me/cybercontroll/Quarry/Quarrymain.java
@@ -12,43 +12,64 @@ public class Quarrymain extends JavaPlugin {
 public final Logger logger = Logger.getLogger("Minecraft");
 
 
-public void onEnable(){
-	//Quarry
-	ItemStack bedrock = new ItemStack(Material.CHEST, 1);
+public void onEnable() {
 	
-	ShapedRecipe Quarry = new ShapedRecipe(bedrock);
+	addRecipes();
+	PluginDescriptionFile pdfFile = this.getDescription();
+	this.logger.info(pdfFile.getName() + "has been enabled");
+	Logger.getLogger("Minecraft").info("Quarry has been enabled");
+	}
+
+	public void onDisable() {
+		PluginDescriptionFile pdffile = this.getDescription();
+		this.logger.info(pdffile.getName() + "has been disabled");	
+	}
 	
-	Quarry.shape("DOD",
-				 "SFT",
-				 "DCD");
-	 
-	Quarry.setIngredient('D', Material.DIAMOND);
-	Quarry.setIngredient('O', Material.OBSIDIAN );
-	Quarry.setIngredient('F', Material.FURNACE);
-	//CHANGE TO DRILL BIT
-	Quarry.setIngredient('S', Material.PRISMARINE_SHARD);
-	Quarry.setIngredient('T', Material.REDSTONE_TORCH_ON);
-	Quarry.setIngredient('C', Material.CHEST);
-	 
-	getServer().addRecipe(Quarry);
-	//Path marker
-	ItemStack Path_marker = new ItemStack(Material.SKULL, 1);
-	
-	ShapedRecipe Marker = new ShapedRecipe(Path_marker);
-	
-	Marker.shape("TTT",
-			 	 "LLL",
-			 	 "TTT");
-	Marker.setIngredient('T', Material.REDSTONE_TORCH_ON);
-	Marker.setIngredient('L', Material.LAPIS_BLOCK);
-	
-	getServer().addRecipe(Marker);
-PluginDescriptionFile pdffile = this.getDescription();
-this.logger.info(pdffile.getName() + "has been enabled");
-}
-public void onDisable(){
-	PluginDescriptionFile pdffile = this.getDescription();
-	this.logger.info(pdffile.getName() + "has been disabled");	
-}
+	public void addRecipes() {
+		//Quarry
+		
+		ItemStack quarryBlock = new ItemStack(Material.BEDROCK, 1);
+		ShapedRecipe Quarry = new ShapedRecipe(quarryBlock);
+		
+		Quarry.shape("DOD",
+					 "SFT",
+					 "DCD");
+		 
+		Quarry.setIngredient('D', Material.DIAMOND);
+		Quarry.setIngredient('O', Material.OBSIDIAN );
+		Quarry.setIngredient('F', Material.FURNACE);
+		//CHANGE TO DRILL BIT
+		Quarry.setIngredient('S', Material.PRISMARINE_SHARD);
+		Quarry.setIngredient('T', Material.REDSTONE_TORCH_ON);
+		Quarry.setIngredient('C', Material.CHEST);
+		 
+		//Path marker									this doesnt work?
+		ItemStack Path_marker = new ItemStack(Material.REDSTONE_TORCH_OFF,1);
+		
+		 ShapedRecipe Marker = new ShapedRecipe(Path_marker);
+		Marker.shape("   ",
+					 "L  ",
+					 "T  ");
+		Marker.shape("   ",
+				 	 " L ",
+					 " T ");
+		Marker.shape("   ",
+			 	 	 "  L",
+				 	 "  T");
+		Marker.shape("L  ",
+		 	 		 "T  ",
+					 "   ");
+		Marker.shape(" L ",
+					 " T ",
+					 "   ");
+		Marker.shape("  L",
+				  	 "  T",
+					 "   ");
+		Marker.setIngredient('L', Material.LAPIS_BLOCK);
+		Marker.setIngredient('T', Material.REDSTONE_TORCH_ON);
+		
+		getServer().addRecipe(Quarry);
+		getServer().addRecipe(Marker);
+	}
 
 }
