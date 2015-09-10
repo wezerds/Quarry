5fd9b7304dcda455ac009a3dac3ead9b765e9902
electro <electrohedric@null.spigotmc.org>
2015-09-09 22:00:41 -0400

    updates and fixes
    fixed plugin.yml error
    fixed redstone torch error
    revised recipes
    named items crafted with red color

--- src/me/cybercontroll/Quarry/Quarrymain.java
+++ src/me/cybercontroll/Quarry/Quarrymain.java
@@ -2,33 +2,34 @@ package me.cybercontroll.Quarry;
 
 import java.util.logging.Logger;
 
+import net.md_5.bungee.api.ChatColor;
+
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.ShapedRecipe;
-import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Quarrymain extends JavaPlugin {
 public final Logger logger = Logger.getLogger("Minecraft");
 
 
-public void onEnable() {
+	public void onEnable() {
 	
 	addRecipes();
-	PluginDescriptionFile pdfFile = this.getDescription();
-	this.logger.info(pdfFile.getName() + "has been enabled");
 	Logger.getLogger("Minecraft").info("Quarry has been enabled");
 	}
 
 	public void onDisable() {
-		PluginDescriptionFile pdffile = this.getDescription();
-		this.logger.info(pdffile.getName() + "has been disabled");	
+		Logger.getLogger("Minecraft").info("Quarry has been disabled");
 	}
-	
+	/**
+	 * Adds all the recipes from the Quarry plugin
+	 */
 	public void addRecipes() {
 		//Quarry
 		
 		ItemStack quarryBlock = new ItemStack(Material.BEDROCK, 1);
+		quarryBlock.getItemMeta().setDisplayName(ChatColor.RED.toString() + "Quarry");
 		ShapedRecipe Quarry = new ShapedRecipe(quarryBlock);
 		
 		Quarry.shape("DOD",
@@ -43,28 +44,13 @@ public void onEnable() {
 		Quarry.setIngredient('T', Material.REDSTONE_TORCH_ON);
 		Quarry.setIngredient('C', Material.CHEST);
 		 
-		//Path marker									this doesnt work?
-		ItemStack Path_marker = new ItemStack(Material.REDSTONE_TORCH_OFF,1);
+		//Path marker
+		ItemStack marker = new ItemStack(Material.REDSTONE_TORCH_ON,1);
+		marker.getItemMeta().setDisplayName(ChatColor.RED.toString() + "Path Marker");
 		
-		 ShapedRecipe Marker = new ShapedRecipe(Path_marker);
-		Marker.shape("   ",
-					 "L  ",
-					 "T  ");
-		Marker.shape("   ",
-				 	 " L ",
-					 " T ");
-		Marker.shape("   ",
-			 	 	 "  L",
-				 	 "  T");
-		Marker.shape("L  ",
-		 	 		 "T  ",
-					 "   ");
-		Marker.shape(" L ",
-					 " T ",
-					 "   ");
-		Marker.shape("  L",
-				  	 "  T",
-					 "   ");
+		ShapedRecipe Marker = new ShapedRecipe(marker);
+		Marker.shape("L",
+					 "T");
 		Marker.setIngredient('L', Material.LAPIS_BLOCK);
 		Marker.setIngredient('T', Material.REDSTONE_TORCH_ON);
 		
